ebb7c6a50ef69ff3bfa405c0b464571e71441137
ZINKEVICH Maksim <drmvz@Imail.ru>
2023-05-22 18:13:56 +0300

    fin_22_05_2023

--- Program.cs
+++ Program.cs
@@ -1,5 +1,16 @@
-﻿int numberA = new Random().Next(1,10);
-int numberB = new Random().Next(1,10);
-int result = numberA + numberB;
-Console.WriteLine(result);
+﻿// int numberA = new Random().Next(1,10);
+// int numberB = new Random().Next(1,10);
+// int result = numberA + numberB;
+// Console.WriteLine(result);
 
+Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+if (username.ToLower() == "Максим")
+{
+Console.WriteLine(" Это же МАКСИМ");
+}
+else
+{
+Console.Write(" Привет ");
+Console.Write(username );
+}
