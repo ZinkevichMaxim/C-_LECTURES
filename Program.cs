d145a7da09ed64b7f1fa625338fb9f50540240fa
ZINKEVICH Maksim <drmvz@Imail.ru>
2023-05-23 14:49:00 +0300

    23/05/2023

--- Program.cs
+++ Program.cs
@@ -18,11 +18,11 @@
 ﻿// task5 - (семинар1) - Напишите программу, которая на вход принимает одно число (N),
 //  а на выходе показывает все целые числа в промежутке от -N до N
 
-Console.WriteLine("Введите число");
-int x = int.Parse(Console.ReadLine());
-if(x < 0)
-{x = x * -1;}
-int z = x * -1;
-do {Console.Write(z + "/");
-    z = z + 1;}
-while (z <= x);
+// Console.WriteLine("Введите число");
+// int x = int.Parse(Console.ReadLine());
+// if(x < 0)
+// {x = x * -1;}
+// int z = x * -1;
+// do {Console.Write(z + "/");
+    // z = z + 1;}
+// while (z <= x);
