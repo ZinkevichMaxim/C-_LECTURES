0277a1d0d77150ee4a6385336068fe84f0056704
ZINKEVICH Maxim <drmvz@mail.ru>
2023-05-24 22:16:18 +0300

    program_fin

--- C#HW_1/Task_plus/Program.cs
+++ C#HW_1/Task_plus/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.WriteLine("Введите любое целое число");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите любое целое число");
 int number = int.Parse(Console.ReadLine());
 
 if (number > 999)
@@ -14,10 +19,10 @@ if (number < 100)
     Console.WriteLine("Третьей цифры нет");
 }
 
-else // (number <= 999)
+else 
 {
 int FirstDigit = number / 100;
-int flagnumber = number - FirstDigit*100; // Получили десятки и единицы
+int flagnumber = number - FirstDigit*100; // как вариант flagnumber = number / 10; SecondDigit = flagnumber % 10; (так лучше)
 int SecondDigit = flagnumber / 10;
 Console.WriteLine(SecondDigit);
 }
