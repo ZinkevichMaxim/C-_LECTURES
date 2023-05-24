93c2c943e1944e0f25d2f1ec0ea3bc777f7778b9
ZINKEVICH Maxim <drmvz@mail.ru>
2023-05-24 22:14:00 +0300

    program_fin

--- C#HW_1/Task 13/Program.cs
+++ C#HW_1/Task 13/Program.cs
@@ -3,29 +3,26 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-// else if (number <= 999)
-// {
-//     int FirstDigit = number / 100;
-// int flagnumber = number - FirstDigit*100; // Получили десятки и единицы
-// int SecondDigit = flagnumber / 10;
-// Console.WriteLine(SecondDigit);
-// }
-
 Console.WriteLine("Введите любое целое число");
 int number = int.Parse(Console.ReadLine());
 
-while (number > 999) do
+if (number > 999)
 {
-    number = number % 10;
+    while (number > 999)
+    {
+        number = number / 10;
+        }
 }
+
 if (number < 100)
 {
     Console.WriteLine("Третьей цифры нет");
 }
-else
+
+else 
 {
-    int FirstDigit = number / 100;
-    int flagnumber = number - FirstDigit*100; // Получили десятки и единицы
-    int SecondDigit = flagnumber / 10;
+int FirstDigit = number / 100;
+int flagnumber = number - FirstDigit*100; // как вариант flagnumber = number / 10; SecondDigit = flagnumber % 10; (так лучше)
+int SecondDigit = flagnumber / 10;
 Console.WriteLine(SecondDigit);
 }
