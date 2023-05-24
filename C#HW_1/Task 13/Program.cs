e123d2cfc3f532579c3879119c7035a3ff7e42e6
ZINKEVICH Maksim <drmvz@Imail.ru>
2023-05-24 16:46:57 +0300

    program before 999

---
+++ C#HW_1/Task 13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите любое целое число");
+int number = int.Parse(Console.ReadLine());
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else if (number <= 999)
+{
+    int FirstDigit = number / 100;
+int flagnumber = number - FirstDigit*100; // Получили десятки и единицы
+int SecondDigit = flagnumber / 10;
+Console.WriteLine(SecondDigit);
+}
