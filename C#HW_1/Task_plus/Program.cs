2d8911733b3b69229684b64ce5161ec25c2a46fd
ZINKEVICH Maxim <drmvz@mail.ru>
2023-05-24 21:59:32 +0300

    prigram_3

---
+++ C#HW_1/Task_plus/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите любое целое число");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+if (number > 999)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+        }
+}
+
+if (number <= 999)
+{
+int FirstDigit = number / 100;
+int flagnumber = number - FirstDigit*100; // Получили десятки и единицы
+int SecondDigit = flagnumber / 10;
+Console.WriteLine(SecondDigit);
+}
+
+
+
+
+
