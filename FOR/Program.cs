bda2cceab3080a196a606380365ed0d02b0bae2a
ZINKEVICH Maxim <drmvz@mail.ru>
2023-06-04 13:53:47 +0300

    lec_3

--- FOR/Program.cs
+++ FOR/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Clear();
+
+// 
+// for(int i = 0; i < 10; i++)
+// {
+//     Console.WriteLine("Hwlo!");
+// }
+
+// string Metod4(int count, string Text)       //    char = обуглить
+// {
+//     int i = 0;
+//     string result = String.Empty;
+//     while(i < count)
+//     {
+//         result = result + Text;
+//         i++;
+//     }
+//     return result;
+// }
+
+// //ВЫЗОВ
+// string res = Metod4(10, "qwer");
+// Console.WriteLine(res);
+
+// string Metod4(int count, string Text)       //    char = обуглить
+// {
+//     string result = String.Empty;
+//     //инициализация счутчика; условие;  инкремент
+//     for(int i = 0;           i < count; i++)
+//     {
+//         result = result + Text;
+//     }
+//     return result;
+// }
+
+// //ВЫЗОВ
+// string res = Metod4(10, "qwer");
+// Console.WriteLine(res);
+
+// ЦИКЛ В ЦИКЛЕ
+
+// Таблица умножения
+
+// for(int i = 2; i < 10; i++)
+// {
+//     for(int j = 2; j < 10; j++)
+//     {
+//     Console.WriteLine($"{i} x {j} = {i*j}");
+//     }
+//     Console.WriteLine();
+// }
