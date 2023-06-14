c68f71331dfe3b8af9eaddf932404bb417356e12
ZINKEVICH Maksim <drmvz@Imail.ru>
2023-06-14 15:44:17 +0300

    fin_fin

--- C#HW_7/Task_47/Program.cs
+++ C#HW_7/Task_47/Program.cs
@@ -8,64 +8,6 @@
 // 8 7,8 -7,1 9
 
 
-// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
-// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
-
-
-
-// double[] GetArray(int len, int minValue, int maxValue)
-// {
-//     double [] arr = new double[len];
-//     for(int i = 0; i < len; i++)
-//     {
-//         arr[i] = Convert.ToDouble(new Random().Next(minValue, maxValue)) / 100;
-//     }
-//     return arr;
-// }
-
-// void PrintArray2(double[] aarr)
-// {
-//     for(int i = 0; i < aarr.Length; i++)
-//     {
-//         Console.Write($"{aarr[i]}, ");
-//     }
-// }
-
-
-// void PrintArray(double[] aarr)
-// {
-//     for(int i = 0; i < aarr.Length; i++)
-//     {
-//         Console.Write($"{aarr[i]}, ");
-//     }
-// }
-
-
-// void FindMinMax(double[] arrr)
-// {
-//     double min = arrr[0];
-//     double max = arrr[0];
-//     for(int i = 0; i < arrr.Length; i++)
-//     {
-//         if(min > arrr[i])
-//         {
-//             min = arrr[i];
-//         }
-//         if (max < arrr[i])
-//         {
-//             max = arrr[i];
-//         }
-//     }
-//         Console.Write($"Минимальный элемент в цикле = {min}. Максимальный элемент в цикле = {max}. Разниуа между максимальным и минимальным элементами сотавляет {max - min}");
-    
-// }
-
-// double[] array = GetArray(10, -1258, 8);
-// PrintArray2(array);
-// Console.WriteLine();
-// FindMinMax(array);
-// Console.WriteLine();
-
 int rows = Prompt("Введите колличество строк массива");
 int columns = Prompt("Введите колличество столбцов массива");
 int minValue = Prompt("Введите минимальное значение в масиве");
