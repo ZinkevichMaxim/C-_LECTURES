99c0c72a8c804f98c3293d9036e3c7e8267f3227
ZINKEVICH Maksim <drmvz@Imail.ru>
2023-06-14 16:57:12 +0300

    fin

--- C#HW_7/Task_50/Program.cs
+++ C#HW_7/Task_50/Program.cs
@@ -6,3 +6,63 @@
 // 8 4 2 4
 // [1,7] -> такого числа в массиве нет [1,7]-это позиция элемента 1 - строка, 7 - столбец
 
+Console.Clear();
+
+
+int Prompt(string msg)
+{
+    Console.WriteLine(msg);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[,] GetArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            result[i,j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        Console.Write($"{array[i,j]}; ");
+        Console.WriteLine();
+    }
+    
+}
+
+
+int rows = Prompt("Введите колличество строк в массиве ");
+int columns = Prompt("Введите колличество столбцов в массиве ");
+int minValue = Prompt("Введите минимальное значение в массиве ");
+int maxValue = Prompt("Введите максимальное значение в массиве ");
+
+int [,] array = GetArray(rows, columns, minValue, maxValue);
+PrintArray(array);
+
+int Elemrow = Prompt("Введите строку, на котором находится искомый элемент ");
+int Elemcolumn = Prompt("Введите столбец, в котром находится искомый элесент");
+FindElem(array);
+
+
+void FindElem(int[,] array)
+{
+if(Elemrow < rows && Elemcolumn < columns)
+{
+    Console.WriteLine($"Значение искомого элемента равно {array[Elemrow, Elemcolumn]}");
+}
+else
+{
+    Console.WriteLine($"Такого элемента в массивен нет ");
+}
+}
+
